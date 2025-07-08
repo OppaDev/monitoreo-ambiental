70e7950bd37c5aaa22c7805063cc68ee301216c7
OppaDev <jlobando2@espe.edu.ec>
2025-07-07 19:48:56 -0500

    changes low

--- NotificationDispatcher/src/main/java/ec/edu/espe/NotificationDispatcher/controller/NotificationController.java
+++ NotificationDispatcher/src/main/java/ec/edu/espe/NotificationDispatcher/controller/NotificationController.java
@@ -24,7 +24,7 @@ import java.util.UUID;
  * Proporciona endpoints para consultar logs y estadísticas
  */
 @RestController
-@RequestMapping("notifications")
+@RequestMapping("/notifications")
 @Slf4j
 public class NotificationController {
 
